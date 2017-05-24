f41ddd27677f0c3735e2e67ad80f9182e9113728
xsi640 <xsi640@hotmail.com>
2017-05-24 16:49:04 +0800

    等待加載頁面，吃豆人。

--- src/renderer/loading/index.js
+++ src/renderer/loading/index.js
@@ -1,1 +1,21 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import ReactLoaders from './react-loaders'
 import './index.scss'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="tips">
+                    Please wait...loading...
+                </div>
+                <div className="react-loaders">
+                    <ReactLoaders type="pacman"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
