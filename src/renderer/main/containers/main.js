95a03b1ca62651ceb07cf773b452359e512b902a
xsi640 <xsi640@hotmail.com>
2017-05-27 22:56:24 +0800

    notebook\tag图标

--- src/renderer/main/containers/main.js
+++ src/renderer/main/containers/main.js
@@ -87,7 +87,7 @@ class Main extends Component {
                     <SplitPane split="horizontal" minSize={300} defaultSize={400} maxSize={-200} className="left">
                         <div className="notebook">
                             <div className="title unselect">
-                                <img className="mynb_icon" src={require('../images/notebook.svg')}></img>My Notebook
+                                <div className="mynb_icon"></div>My Notebook
                                 <Button shape="circle" onClick={(e) => {
                                     this.refs.notebookModal.getWrappedInstance().show(null, null)
                                 }}>Add</Button>
@@ -101,7 +101,9 @@ class Main extends Component {
                             </div>
                         </div>
                         <div className="tags">
-                            <div className="title unselect">Tags</div>
+                            <div className="title unselect">
+                                <div className="mytag_icon"></div>Tags
+                            </div>
                             <Tags ref="tags" onSelected={this.handleSelectTag}/>
                         </div>
                     </SplitPane>
